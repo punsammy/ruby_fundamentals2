21d42e399686881fa3233444db4089a73975f140
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-29 14:18:27 -0400

    exercise9.1

--- exercise9.rb
+++ exercise9.rb
@@ -1,1 +1,5 @@
-grocery_list = ["grapes", "apples", "carrots", "tea", "bagels", "mangoes"]
+grocery_list = ["grapes", "apples", "bagels", "mangoes"]
+
+grocery_list.each do | grocery_list |
+  puts "*#{ grocery_list }"
+end

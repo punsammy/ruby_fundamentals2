ec53b1b3f65d2a8ca45f34812a32dd07c30c4378
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-29 16:37:47 -0400

    exercise10

--- exercise10.rb
+++ exercise10.rb
@@ -4,7 +4,53 @@ students = {
   :cohort3 => 22
 }
 
-students.each do |key, value|
-  new_value = value + 10
-  students[key] = value
+#displaying cohorts like this  cohortX : y students
+def list_cohorts(students)
+  students.each do |key, value|
+    puts "#{key} : #{value} students"
+  end
 end
+
+#Adding cohort 4 with 43 students
+students[:cohort4] = 43
+
+#display new list
+list_cohorts(students)
+
+#display all keys (cohort names)
+puts students.keys
+
+#expand cohorts by 5%
+students.each do |key, value|
+  students[key] = value*1.05
+  puts "#{key} increases to #{students[key]}"
+end
+
+#delete 2nd cohort and display hash
+students.delete(:cohort2)
+
+list_cohorts(students)
+
+#Total number of students across all cohorts
+
+puts students.merge!(students)
+
+total = 0
+students.each do |key, value|
+  total = total + students[key]
+  puts "Total is #{total} students"
+end
+
+
+
+  # BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
+  #
+  # BONUS: Create another similar hash called staff and display it using the same method.
+  #
+  # After you're done, be sure you have committed and pushed everything to your Github repo.
+
+
+# students.each do |key, value|
+#   new_value = value + 10
+#   students[key] = value
+# end

0c74854b246c833ed51265fa732439972f4d83ee
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-29 14:57:10 -0400

    exercise9.2 and 9.3

--- exercise9.rb
+++ exercise9.rb
@@ -1,5 +1,21 @@
+#Grocery List
 grocery_list = ["grapes", "apples", "bagels", "mangoes"]
 
+#Adding rice to list
+grocery_list.push( 'rice' )
+
+#Making each of the grocery list items display with *
 grocery_list.each do | grocery_list |
   puts "*#{ grocery_list }"
+  #puts "* " +grocery_list
+end
+
+#Stating number of items on list
+puts "There are #{grocery_list.length } items you need to get"
+
+#Checking array for specific item
+if grocery_list.include?('banana')
+  puts "Hooray monkeys will be happy"
+else
+  puts "You forgot bananas. Monkeys will not be happy"
 end
